8d5655e214c071f5abc09af22304e17d96ae978a
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2022-05-24 13:19:44 +0900

    Delete week4 directory

--- week4/blogproject/blogproject/blogapp/forms.py
+++
@@ -1,8 +0,0 @@
-#장고에서 제공하는 폼기능 구현
-from django import forms
-from .models import Blog
-
-class BlogForm(forms.Form):
-    #입력받고자 하는 값들
-    title = forms.CharField()
-    body=forms.CharField(widget=forms.Textarea)
